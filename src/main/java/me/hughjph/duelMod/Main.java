20caf2db9173014dcc152cf94d775534bf6dad0d
hughjph <hughjohnson2018@outlook.com>
2021-02-18 19:59:00 +0000

    untested changes, accept challenge

--- src/main/java/me/hughjph/duelMod/Main.java
+++ src/main/java/me/hughjph/duelMod/Main.java
@@ -1,13 +1,16 @@
 package me.hughjph.duelMod;
 
 import me.hughjph.duelMod.Commands.duelPlayer;
+import me.hughjph.duelMod.events.accept;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 
     @Override
     public void onEnable(){
+
         getCommand("duel").setExecutor(new duelPlayer());
+        getServer().getPluginManager().registerEvents(new accept(), this);
     }
 
 }
