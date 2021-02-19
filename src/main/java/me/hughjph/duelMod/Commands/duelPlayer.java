e0c75603a63d57a30e64fb920ad9d949643cad4d
hughjph <hughjohnson2018@outlook.com>
2021-02-19 01:29:21 +0000

    gets coords - major bug with finding challenged players from the array

--- src/main/java/me/hughjph/duelMod/Commands/duelPlayer.java
+++ src/main/java/me/hughjph/duelMod/Commands/duelPlayer.java
@@ -1,15 +1,23 @@
 package me.hughjph.duelMod.Commands;
 
 import com.jcabi.aspects.Async;
+import jdk.nashorn.internal.parser.JSONParser;
 import me.hughjph.duelMod.classes.Duel;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
 
 public class duelPlayer implements CommandExecutor {
 
@@ -34,7 +42,7 @@ public class duelPlayer implements CommandExecutor {
             if(args.length == 1){
                 Player targetPlayer = p.getServer().getPlayer(args [0]);
                 p.sendMessage("Sent duel request");
-                targetPlayer.sendMessage(p.getName() + " has challenged you to a duel! Type /a to accept");
+                targetPlayer.sendMessage(p.getName() + " has challenged you to a duel! Type a to accept");
                 challengedPlayers.add(new Duel(targetPlayer.getName(), p.getName()));
                 challengerCountdown(targetPlayer, p);
             }
@@ -62,12 +70,57 @@ public class duelPlayer implements CommandExecutor {
 
     }
 
-
+    @SuppressWarnings("unchecked")
     public static void DuelStart(Player Challenger, Player Challenged){
+        org.json.simple.parser.JSONParser parser;
+        parser = new org.json.simple.parser.JSONParser();
+        boolean coords1got = false;
+        Location p1Spawn;
+        Location p2Spawn;
 
+        try (FileReader reader = new FileReader("../../../../../../coordinates.json")) {
+            Object obj = parser.parse(reader);
+
+            JSONArray coords = (JSONArray) obj;
+
+            for (int i = 0; i < coords.size(); i++){
+                JSONObject coordObj = (JSONObject) coords.get(i);
+
+                Integer X = (int) coordObj.get("x");
+                Integer Y = (int) coordObj.get("y");
+                Integer Z = (int) coordObj.get("z");
+
+                if(coords1got){
+                    p2Spawn =  new Location (Challenged.getWorld(), X, Y, Z);
+                    Challenged.sendMessage("You will teleport to here " + p2Spawn);
+
+                } else{
+                    p1Spawn = new Location (Challenged.getWorld(), X, Y, Z);
+                    Challenger.sendMessage("You will teleport to here " + p1Spawn);
+
+                    coords1got = true;
+                }
+            }
+
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 
+    private static void parseCoords(JSONObject coord){
+        JSONObject coordObj = (JSONObject) coord.get("Player");
 
+        Integer X = (int) coordObj.get("x");
+        Integer Y = (int) coordObj.get("y");
+        Integer Z = (int) coordObj.get("z");
+
+    }
 
 }
 
